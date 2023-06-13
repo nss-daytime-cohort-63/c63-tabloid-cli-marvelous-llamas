274b39fc43702cf18f8bca0a3c1e4b2c584d8b7d
John Brady <johntbrady01@gmail.com>
2023-06-13 12:56:16 -0400

    Journal.cs-JournalRepo.cs

---
+++ TabloidCLI/Models/Journal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TabloidCLI.Models
+{
+	public class Journal
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
+
