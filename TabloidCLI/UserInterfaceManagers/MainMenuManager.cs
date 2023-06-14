9ae7ad0054690fcf66939d084fbf0e7f1c474764
John Brady <johntbrady01@gmail.com>
2023-06-14 16:49:38 -0400

    main

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -5,7 +5,7 @@ namespace TabloidCLI.UserInterfaceManagers
     public class MainMenuManager : IUserInterfaceManager
     {
         private const string CONNECTION_STRING =
-            @"Data Source=localhost\SQLEXPRESS07;Database=TabloidCLI;Integrated Security=True";
+            @"Server=127.0.0.1; Database=TabloidCLI; User Id=sa; Password=MyPass@word;integrated security=true;TrustServerCertificate=true; Trusted_Connection=false;";
 
         public IUserInterfaceManager Execute()
         {
